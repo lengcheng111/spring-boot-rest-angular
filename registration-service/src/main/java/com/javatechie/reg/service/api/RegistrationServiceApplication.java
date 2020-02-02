398dbeeafe1687b465e4a7f0970680b27beff90e
truonglv <truonglv174@gmail.com>
2020-02-02 21:56:01 +0700

    first commit

---
+++ registration-service/src/main/java/com/javatechie/reg/service/api/RegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.reg.service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+
+}
